a7c2b912ecc0247853b8a93cb7e6f29d15317421
VedranSelak <selakvedran@gmail.com>
2021-05-28 19:31:30 +0200

    added fragment switching

--- app/src/main/java/com/example/tutorme/MainActivity.java
+++ app/src/main/java/com/example/tutorme/MainActivity.java
@@ -1,14 +1,64 @@
 package com.example.tutorme;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.lifecycle.Lifecycle;
+import androidx.lifecycle.LifecycleObserver;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+    private ViewPager2 viewPager2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navbar);
+        bottomNavigationView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener);
+
+        viewPager2 = findViewById(R.id.fragment_container);
+
+        setUpAdapter(viewPager2);
     }
+
+    private void setUpAdapter(ViewPager2 viewPager2){
+        ViewPageAdapter viewPageAdapter = new ViewPageAdapter(this);
+        viewPageAdapter.addFragment(new HomeFragment());
+        viewPageAdapter.addFragment(new AccountFragment());
+        viewPageAdapter.addFragment(new SettingsFragment());
+        viewPager2.setAdapter(viewPageAdapter);
+    }
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener onNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Log.d("Debug", "Item clicked" + item.getItemId());
+            switch (item.getItemId()) {
+                case R.id.nav_home:
+                    viewPager2.setCurrentItem(0);
+                    return true;
+                case R.id.nav_account:
+                    viewPager2.setCurrentItem(1);
+                    return true;
+                case R.id.nav_settings:
+                    viewPager2.setCurrentItem(2);
+                    return true;
+                default:
+                    return false;
+            }
+        }
+    };
 }
---
+++ app/src/main/java/com/example/tutorme/ViewPageAdapter.java
@@ -0,0 +1,37 @@
+package com.example.tutorme;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ViewPageAdapter extends FragmentStateAdapter {
+
+    private List<Fragment> fragmentList = new ArrayList<Fragment>();
+
+    public ViewPageAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    public void addFragment(Fragment fragment){
+        this.fragmentList.add(fragment);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        return this.fragmentList.get(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.fragmentList.size();
+    }
+}
