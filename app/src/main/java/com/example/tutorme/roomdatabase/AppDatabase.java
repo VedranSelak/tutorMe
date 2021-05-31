d2f8ae77300c743714e65d26b8b5c8b6a75edf21
VedranSelak <selakvedran@gmail.com>
2021-05-31 23:50:51 +0200

    added room database and activity for registration

---
+++ app/src/main/java/com/example/tutorme/roomdatabase/AppDatabase.java
@@ -0,0 +1,23 @@
+package com.example.tutorme.roomdatabase;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {UserEntity.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+    private static final String dbName = "tutorMe_db";
+    public abstract UserDao userDao();
+    private static AppDatabase appDatabase;
+
+    public static synchronized AppDatabase getAppDatabase(Context context){
+        if(appDatabase == null) {
+            appDatabase = Room.databaseBuilder(context, AppDatabase.class, dbName).fallbackToDestructiveMigration().build();
+        }
+        return appDatabase;
+    }
+
+
+}
---
+++ app/src/main/java/com/example/tutorme/roomdatabase/UserEntity.java
@@ -0,0 +1,53 @@
+package com.example.tutorme.roomdatabase;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
+public class UserEntity {
+
+    @PrimaryKey(autoGenerate = true)
+    public int id;
+
+    @ColumnInfo(name = "email")
+    String userEmail;
+
+    @ColumnInfo(name = "password")
+    String userPassword;
+
+    @ColumnInfo(name = "fullName")
+    String userFullName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public String getUserFullName() {
+        return userFullName;
+    }
+
+    public void setUserFullName(String userFullName) {
+        this.userFullName = userFullName;
+    }
+}
